b0ba651c4f060991ec3a0241148b7c506f6c54b2
nasmolin <79275016+nasmolin@users.noreply.github.com>
2024-09-03 18:19:45 +0300

    add logger

--- create_dump.sh
+++ create_dump.sh
@@ -1,46 +1,71 @@
+declare -A levels=([DEBUG]=0 [INFO]=1 [WARN]=2 [ERROR]=3)
+script_logging_level="DEBUG"
+host_name=$(cat /etc/hosts | grep 127.0.1.1 |awk '{print $2}')
 discord_webhook_url=''
-now=$(date +"%d.%m.%Y-%H:%M:%S")
-node_env=dev
+script_start_timestamp=$(date +"%d.%m.%Y-%H:%M:%S")
 workdir=/etc/postgresql/14/main/dumper
 logfile=$workdir/dumper.log
 dumpsdir=$workdir/dumps
-dumpfile=$workdir/dumps/$now.dump
+dumpfile=$workdir/dumps/$script_start_timestamp.dump
+
+logThis() {
+        local log_message=$1
+        local log_priority=$2
+        [[ ${levels[$log_priority]} ]] || return 1
+        (( ${levels[$log_priority]} < ${levels[$script_logging_level]} )) && return 2
+        echo "${log_priority} $(date +"%d.%m.%Y-%H:%M:%S") ${host_name} ${log_message}" 1>> $logfile
+}
 
 function notify_if_error {
-
-        message_header="Новый инцидент: ошибка при создании бекапа баз данных $node_env окружения"
-        message_body=$(tail -n 1 $logfile | tr -d '\"')
+        message_header="Новый инцидент: ошибка при создании бекапа баз данных на сервере ${host_name}"
+        message_body=$(tail -n 5 $logfile | tr -d '\"' | tr -s '\r\n' ' ' )
         discord_error_header=\"$message_header\"
         discord_error_body=\"\`$message_body\`\"
-
         curl \
         -X POST \
         -H "Content-Type: application/json" \
-        -d "{\"content\": $discord_error_header,\"embeds\": [{\"title\": $discord_error_body}]}" \
+        -d "{\"content\": $discord_error_header,\"embeds\": [{\"title\": \"Output\",\"description\": $discord_error_body}]}" \
         $discord_webhook_url
-        }
-
-function check_exec {
         if [ $? -eq 0 ]; then
-                echo "[SUCCESS]"
+                logThis "Notification was send, exiting." "DEBUG"
+                cat $logfile
+                exit 1
         else
-                echo "[ERROR] Trying to send notification."
-                notify_if_error
+                logThis "Unable to notify Discord." "ERROR"
+                cat $logfile
                 exit 1
         fi
 }
 
-echo "[DEBUG] Trying to create some dir..."
+function check_exec {
+        if [ $? -eq 0 ]; then
+                logThis "Success." "INFO"
+        else
+                logThis "Command exit with errors." "ERROR"
+                logThis "Trying to send notification." "DEBUG"
+                notify_if_error
+        fi
+}
+
+rm $logfile
+if ! [[ -f ${logfile} ]]; then
+        echo -e "[INFO] No log file located, creating new log file (${logfile})."
+        echo -e "All logs will be redirecte to this file."
+        logThis "Log file created." "INFO"
+fi
+logThis "Script initiated" "DEBUG"
+logThis "Trying to create empty .dump file." "DEBUG"
 touch $dumpfile >> $logfile 2>> $logfile
 check_exec
 
-echo "[DEBUG] Trying to change permissions..."
+logThis "Trying to change permissions." "DEBUG"
 chown -R postgres:postgres $workdir >> $logfile 2>> $logfile
 check_exec
 
-echo "[DEBUG] Trying to create dump..."
-echo "[DEBUG] Dump will be saved in $dumpfile"
-sudo -u postgres pg_dumpall -f $dumpfile >> $logfile 2>>$logfile
+logThis "Trying to create dump." "DEBUG"
+logThis "Dump will be saved in ${dumpfile}." "DEBUG"
+sudo -u postgres pg_dumspall -f $dumpfile >> $logfile 2>>$logfile
 check_exec
 
+logThis "Script exit with code 0." "INFO"
 exit 0
