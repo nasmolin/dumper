dcd5076fb2af5f426d47ee0452a54372a69c749b
nasmolin <79275016+nasmolin@users.noreply.github.com>
2024-09-04 13:42:55 +0300

    logrotate added

--- create_dump.sh
+++ create_dump.sh
@@ -1,13 +1,21 @@
+########################################################
+# vars:
+discord_webhook_url=''
+########################################################
 declare -A levels=([DEBUG]=0 [INFO]=1 [WARN]=2 [ERROR]=3)
 script_logging_level="DEBUG"
 host_name=$(cat /etc/hosts | grep 127.0.1.1 |awk '{print $2}')
-discord_webhook_url=''
 script_start_timestamp=$(date +"%d.%m.%Y-%H:%M:%S")
-workdir=/etc/postgresql/14/main/dumper
-logfile=$workdir/dumper.log
+workdir=$(pwd)
+logsdir=$workdir/logs
+logfile=$workdir/logs/pg_dumper.log
 dumpsdir=$workdir/dumps
 dumpfile=$workdir/dumps/$script_start_timestamp.dump
+logrotateonfig=/etc/logrotate.d/pg_dumper.conf
 
+########################################################
+# func:
+########################################################
 logThis() {
         local log_message=$1
         local log_priority=$2
@@ -15,13 +23,13 @@ logThis() {
         (( ${levels[$log_priority]} < ${levels[$script_logging_level]} )) && return 2
         echo "${log_priority} $(date +"%d.%m.%Y-%H:%M:%S") ${host_name} ${log_message}" 1>> $logfile
 }
-
 function notify_if_error {
         message_header="Новый инцидент: ошибка при создании бекапа баз данных на сервере ${host_name}"
         message_body=$(tail -n 5 $logfile | tr -d '\"' | tr -s '\r\n' ' ' )
         discord_error_header=\"$message_header\"
         discord_error_body=\"\`$message_body\`\"
         curl \
+        -f \
         -X POST \
         -H "Content-Type: application/json" \
         -d "{\"content\": $discord_error_header,\"embeds\": [{\"title\": \"Output\",\"description\": $discord_error_body}]}" \
@@ -36,10 +44,9 @@ function notify_if_error {
                 exit 1
         fi
 }
-
 function check_exec {
         if [ $? -eq 0 ]; then
-                logThis "Success." "INFO"
+                logThis "Done." "INFO"
         else
                 logThis "Command exit with errors." "ERROR"
                 logThis "Trying to send notification." "DEBUG"
@@ -47,25 +54,44 @@ function check_exec {
         fi
 }
 
-rm $logfile
+echo -e "Script initiated"
 if ! [[ -f ${logfile} ]]; then
-        echo -e "[INFO] No log file located, creating new log file (${logfile})."
-        echo -e "All logs will be redirecte to this file."
+        echo -e "[INFO] Creating log file: (${logfile})."
+        echo -e "stdout and stderr will be redirected to this file."
         logThis "Log file created." "INFO"
 fi
-logThis "Script initiated" "DEBUG"
-logThis "Trying to create empty .dump file." "DEBUG"
-touch $dumpfile >> $logfile 2>> $logfile
-check_exec
 
-logThis "Trying to change permissions." "DEBUG"
-chown -R postgres:postgres $workdir >> $logfile 2>> $logfile
-check_exec
+########################################################
+# Script:
+########################################################
+if ! [[ $(whoami) == root ]]; then 
+         echo -e "[ERROR] Permission Denied, are you root?"
+         exit 1
+fi
+rm $logrotateonfig
+logThis "Creating logrotate config." "DEBUG"
+cat >${logrotateonfig} <<-EOF
+        ${logfile} {
+        missingok
+        daily
+        rotate 3
+        size 50k
+        }
+EOF
 
+logThis "Apply logrotate config." "DEBUG"
+logrotate ${logrotateonfig} >> $logfile 2>> $logfile && check_exec
+logThis "Trying to create logs dirs." "INFO"
+mkdir -p $logsdir >> $logfile 2>> $logfile && check_exec
+logThis "Trying to create dir for .dump files." "INFO"
+mkdir -p $dumpsdir >> $logfile 2>> $logfile && check_exec
+logThis "Trying to create empty .dump file." "INFO"
+touch $dumpfile >> $logfile 2>> $logfile && check_exec
+logThis "Trying to change permissions." "INFO"
+chown postgres:postgres $dumpfile >> $logfile 2>> $logfile && check_exec
 logThis "Trying to create dump." "DEBUG"
 logThis "Dump will be saved in ${dumpfile}." "DEBUG"
-sudo -u postgres pg_dumspall -f $dumpfile >> $logfile 2>>$logfile
-check_exec
-
-logThis "Script exit with code 0." "INFO"
+sudo -u postgres pg_dumpall -f $dumpfile >> $logfile 2>>$logfile && check_exec
+logThis "Script exit with code 0." "INFO."
+echo -e "[SUCCESS] script exit with code 0."
 exit 0
